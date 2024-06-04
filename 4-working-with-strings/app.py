285425193eb9ce9f734172ceb215e1cea73e7c76
Emek Taydaş <emektaydas@hotmail.com>
2024-06-04 09:44:35 +0300

    working-with-strings

---
+++ 4-working-with-strings/app.py
@@ -0,0 +1,10 @@
+phrase = "Giraffe Academy"
+
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase[3])
+print(phrase.index("G"))
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
