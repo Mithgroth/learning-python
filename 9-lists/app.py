7436f5532d45c9c5d16d35788428e5a90356313a
Emek Taydaş <emektaydas@hotmail.com>
2024-06-04 10:16:00 +0300

    lists

---
+++ 9-lists/app.py
@@ -0,0 +1,10 @@
+friends = [ "Kevin", "Karen", "Jim", "Oscar", "Toby" ]
+
+print(friends)
+print(friends[-2])
+print(friends[1:])
+print(friends[1:3])
+print(friends[:2])
+
+friends[1] = "Mike"
+print(friends[1])
