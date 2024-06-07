f78a7a5c0f8d7d9eb54e99adc3f32a3ba0c694c5
Emek Taydaş <mithgroth@gmail.com>
2024-06-07 12:43:12 +0300

    modules-and-pip

---
+++ 28-modules-and-pip/app.py
@@ -0,0 +1,9 @@
+import useful_tools
+
+print(useful_tools.roll_dice(10))
+
+# https://docs.python.org/3/py-modindex.html
+
+# pip install python-docx
+# lives under <python installation>/lib/site-packages
+import docx
